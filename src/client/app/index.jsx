c2812b63eda8139375f27ceff5a9f0498f79fad5
Mstanton98 <mstanton98@gmail.com>
2017-05-28 13:43:11 -0700

    initial commit

---
+++ src/client/app/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {render} from 'react-dom';
+import Main from './components/Main.jsx';
+
+class App extends React.Component {
+  render() {
+    return <Main />
+  }
+}
+
+render(<App/>, document.getElementById('app'));
